8800d7da62abad6c5eb8185d9d6188d72406d4f1
SuIn Woo <wbo1026@naver.com>
2023-10-23 17:41:44 +0900

    feat: 카카오 로그인 초기 구현 에러 잡아야함

---
+++ Front/cyesfront/src/pages/login/Login.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Login.css';
+import { useKakaoLogin } from '../../hooks/useKakaoLogin';
+
+const REST_API_KEY = "17a1b4d9fcc605cecc82fd7399c0717f"; 
+const REDIRECT_URI = "http://localhost:3000/login/kakao/callback"; 
+const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+const Login = () => {
+    useKakaoLogin(); // 카카오 로그인 Hook 사용
+
+    const naverLogin = async () => {
+        try {
+            // Naver 로그인 로직
+        } catch (error) {
+            console.log("naverLogin error: ", error);
+        }
+    };
+
+    const handleKakaoLogin = () => {
+        window.location.href = KAKAO_AUTH_URI;
+    };
+
+    return (
+        <div className='login-container'>
+            <img className='logo-img' src='/img/cyes_logo_img.png' alt='logo img'></img>
+            <button onClick={handleKakaoLogin} className="login-button kakao-login">카카오로 시작하기</button>
+            <button className="login-button naver-login" onClick={naverLogin}>네이버로 시작하기</button>
+        </div>
+    );
+};
+
+export default Login;
+
--- Front/cyesfront/src/services/loginService.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-type Props = {}
-
-const loginService = (props: Props) => {
-
-}
-
-export default loginService
