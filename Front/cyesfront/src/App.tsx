c470bce911a4cf755f83570cfdf830cacffbb043
subin <qotnqls77@naver.com>
2023-10-24 10:15:35 +0900

    feat: quiz 페이지 구현

--- Front/cyesfront/src/App.tsx
+++ Front/cyesfront/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate  } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Quiz from './pages/quiz/Quiz';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path='/login' element={<Login />}/>
+          <Route path='/quiz' element={<Quiz />}/>
+          
         </Routes>
       </BrowserRouter>
     </div>
