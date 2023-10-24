688944ccdf8583c8e04cbfa31b702a07de338a6e
dntmdqls0912 <dntmdqls0912@gmail.com>
2023-10-24 12:50:24 +0900

    fix: change oauth api response types

--- Server/webserver/src/main/java/com/cyes/webserver/utils/oauth/OauthController.java
+++ Server/webserver/src/main/java/com/cyes/webserver/utils/oauth/OauthController.java
@@ -1,7 +1,5 @@
-package com.cyes.webserver.common.controller;
+package com.cyes.webserver.utils.oauth;
 
-import com.cyes.webserver.common.dto.common.CommonResponse;
-import com.cyes.webserver.common.service.ResponseService;
 import com.cyes.webserver.exception.ExceptionResponse;
 import com.cyes.webserver.utils.oauth.request.KakaoLoginParams;
 import com.cyes.webserver.utils.oauth.request.NaverLoginParams;
@@ -28,7 +26,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/oauth")
 public class OauthController {
     private final OAuthLoginService oAuthLoginService;
-    private final ResponseService responseService;
     private final String SUCCESS = "success";
 
     @Operation(summary = "카카오 로그인", description = "authorization 코드를 받아 카카오 로그인을 한다.")
@@ -38,17 +35,13 @@ public class OauthController {
     })
     @Parameter(name = "memberId", description = "조회할 사용자의 memberId")
     @PostMapping("/login/kakao")
-    public ResponseEntity<CommonResponse<LoginResponse>> loginKakao(@RequestBody KakaoLoginParams params) {
-        return new ResponseEntity<>(
-                responseService.getResponse(oAuthLoginService.login(params)),
-                HttpStatus.OK);
+    public ResponseEntity<LoginResponse> loginKakao(@RequestBody KakaoLoginParams params) {
+        return ResponseEntity.status(HttpStatus.OK).body(oAuthLoginService.login(params));
     }
 
     @PostMapping("/login/naver")
-    public ResponseEntity<CommonResponse<LoginResponse>> loginNaver(@RequestBody NaverLoginParams params) {
-        return new ResponseEntity<>(
-                responseService.getResponse(oAuthLoginService.login(params)),
-                HttpStatus.OK);
+    public ResponseEntity<LoginResponse> loginNaver(@RequestBody NaverLoginParams params) {
+        return ResponseEntity.status(HttpStatus.OK).body(oAuthLoginService.login(params));
     }
 
     @Operation(summary = "로그아웃", description = "회원 탈퇴를 한다.")
@@ -58,10 +51,8 @@ public class OauthController {
     })
     @Parameter(name = "memberId", description = "탈퇴할 사용자의 memberId")
     @DeleteMapping("/logout/{memberId}")
-    public ResponseEntity<CommonResponse<Long>> logout(@PathVariable("memberId") Long memberId) {
-        return new ResponseEntity<>(
-                responseService.getResponse(oAuthLoginService.logout(memberId)),
-                HttpStatus.OK);
+    public ResponseEntity<Long> logout(@PathVariable("memberId") Long memberId) {
+        return ResponseEntity.status(HttpStatus.OK).body(oAuthLoginService.logout(memberId));
     }
 
     @Operation(summary = "refresh 토큰 재발급", description = "memberId와 refresh 토큰을 입력받아 access 토큰과 refresh 토큰을 재발급한다.")
@@ -70,7 +61,7 @@ public class OauthController {
             @ApiResponse(responseCode = "400", description = "bad request operation", content = @Content(schema = @Schema(implementation = ExceptionResponse.class)))
     })
     @PostMapping("/regen")
-    public ResponseEntity<CommonResponse<LoginResponse>> regenerateTokens(@RequestBody RegenTokenReq regenTokenReq) {
-        return new ResponseEntity<>(responseService.getResponse(oAuthLoginService.regenToken(regenTokenReq)), HttpStatus.OK);
+    public ResponseEntity<LoginResponse> regenerateTokens(@RequestBody RegenTokenReq regenTokenReq) {
+        return ResponseEntity.status(HttpStatus.OK).body(oAuthLoginService.regenToken(regenTokenReq));
     }
 }
