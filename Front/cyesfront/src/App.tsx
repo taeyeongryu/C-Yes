888b17442d78b6003205d2673991fa7360a1d0c8
SuIn Woo <wbo1026@naver.com>
2023-10-24 10:30:41 +0900

    feat: 라이브 페이지 생성

--- Front/cyesfront/src/App.tsx
+++ Front/cyesfront/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate  } from 'react-router-dom';
 import './App.css';
 import Login from './pages/login/Login'
+import Live from './pages/live/Live';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path='/login' element={<Login />}/>
+          <Route path='/live' element={<Live />}/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ Front/cyesfront/src/pages/live/Live.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import BottomNav from '../../components/BottomNav'
+
+type Props = {}
+
+const Live = (props: Props) => {
+  return (
+    <div>
+        <BottomNav></BottomNav>
+    </div>
+  )
+}
+
+export default Live
