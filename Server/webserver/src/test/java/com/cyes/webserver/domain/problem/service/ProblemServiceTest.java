b7834d71821c85ba637c129b6a2ef2eadaa6929f
유태영 <tybam@naver.com>
2023-10-26 15:47:48 +0900

    Feat: problem service test

---
+++ Server/webserver/src/test/java/com/cyes/webserver/domain/problem/service/ProblemServiceTest.java
@@ -0,0 +1,38 @@
+package com.cyes.webserver.domain.problem.service;
+
+import com.cyes.webserver.domain.problem.dto.ProblemResponse;
+import com.cyes.webserver.domain.problem.entity.ProblemCategory;
+import com.cyes.webserver.domain.problem.entity.ProblemType;
+import org.assertj.core.groups.Tuple;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class ProblemServiceTest {
+    @Autowired
+    private ProblemService problemService;
+
+
+    @Test
+    @DisplayName("문제 Id List로 문제를 조회할 수 있다.")
+    void findAllProblemByQuiz() {
+        // given
+        List<String> idList = List.of("6539cc2767256908d1e90229");
+        // when
+        List<ProblemResponse> allProblemByQuiz = problemService.findAllProblemByQuiz(idList);
+        // then
+        assertThat(allProblemByQuiz).hasSize(1)
+                .extracting("id" ,"contentResponse.question","contentResponse.answer","category", "type")
+                .containsExactly(
+                        Tuple.tuple("6539cc2767256908d1e90229","컴퓨터에서 실행 중인 프로그램 또는 작업을 무엇이라고 합니까?"
+                                ,"프로세스",ProblemCategory.OS, ProblemType.SHORTANSWER)
+                );
+    }
+
+}
