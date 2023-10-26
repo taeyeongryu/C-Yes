df7728f3d45db1ada744145ae55136cabdcbdbaa
subin <qotnqls77@naver.com>
2023-10-26 16:46:59 +0900

    style: quiz css 수정

--- Front/cyesfront/src/components/RoundCornerBtn.tsx
+++ Front/cyesfront/src/components/RoundCornerBtn.tsx
@@ -11,15 +11,20 @@ type Props = {
     fontColor?: string;
     type?: "button" | "submit" | "reset"; // type 속성 추가
     onClick?: () => void;
+    disabled?: boolean;
 };
 
 const StyledButton = styled.button<Props>`
     width: ${(props) => props.width || "100px"};
     height: ${(props) => props.height || `40px`};
     background-color: ${(props) => props.bgColor || `#31a0eb`};
+    &:active {
+        background-color: ${(props) => props.bgHover || `#32237d`};
+    }
     font-size: ${(props) => props.fontSize || "12px"};
     color: ${(props) => props.fontColor || `#ffffff`};
     border-radius: 18px;
+
     border: none;
 `;
 
