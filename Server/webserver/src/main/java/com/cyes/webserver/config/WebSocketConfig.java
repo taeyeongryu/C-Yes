77c877e9dbee0a1cfd0f135771490b1c73ed1b17
유태영 <tybam@naver.com>
2023-10-30 11:00:07 +0900

    Style: 주석 작성

--- Server/webserver/src/main/java/com/cyes/webserver/config/WebSocketConfig.java
+++ Server/webserver/src/main/java/com/cyes/webserver/config/WebSocketConfig.java
@@ -2,6 +2,8 @@ package com.cyes.webserver.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -23,6 +25,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
 
     @Override // 클라이언트에서 접속할 endpoint 설정
+    @SendTo
+    @SendToUser
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/quiz/session")
                 .setAllowedOriginPatterns("*").withSockJS();
