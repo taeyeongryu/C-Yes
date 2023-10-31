905b6f06ad76132421a4558b68f04bef69f0a12e
SuIn Woo <wbo1026@naver.com>
2023-10-31 10:35:06 +0900

    fix: 충돌 에러

--- Front/cyesfront/src/pages/login/LoginRedir.tsx
+++ Front/cyesfront/src/pages/login/LoginRedir.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { requestKakaoLoginInfo } from "../../api/LoginAPI";
 
 const LoginRedir = () => {
-  const memberState = useSelector((state: any) => state.member);
+  // const memberState = useSelector((state: any) => state.member);
   const oAuthState = useSelector((state: any) => state.oauth);
 
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
   useEffect(() => {
     console.log("메롱")
@@ -25,7 +25,7 @@ const LoginRedir = () => {
     };
 
     Login();
-  }, []);
+  }, [oAuthState]);
 
   return <div>로그인 중 입니다!</div>;
 };
