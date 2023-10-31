44399d73f1a8bd751065808dc49c39057279bd05
dntmdqls0912 <dntmdqls0912@gmail.com>
2023-10-31 13:14:05 +0900

    Feat: ExpirationListner의 Listen Key Filtering 기능 추가, 스케줄링 수정, 삭제 기능 추가

--- Server/webserver/src/main/java/com/cyes/webserver/redisListener/ScheduleReserveService.java
+++ Server/webserver/src/main/java/com/cyes/webserver/redisListener/ScheduleReserveService.java
@@ -1,7 +1,7 @@
 package com.cyes.webserver.redisListener;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.cyes.webserver.exception.CustomException;
+import com.cyes.webserver.exception.CustomExceptionList;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -17,14 +17,26 @@ import java.time.LocalDateTime;
 public class ScheduleReserveService {
 
     private final StringRedisTemplate stringRedisTemplate;
-    private final ObjectMapper objectMapper;
+    private final String SCHEDULE_PREFIX = "ScheduledQuizSession_";
 
 
-    public void save(Long quizId, LocalDateTime expireTime) throws JsonProcessingException {
+    /**
+     *
+     * expire scheduler redis에 prefix + quizID를 한 키값을 저장해 작업을 예약한다.
+     *
+     * @param quizId 스케줄링 할 퀴즈의 ID
+     * @param expireTime 퀴즈 시작 시간 (키 만료 시간)
+     */
+    public void save(Long quizId, LocalDateTime expireTime) {
 
         ValueOperations<String, String> op = stringRedisTemplate.opsForValue();
 
-        String quizIdStr = objectMapper.writeValueAsString(quizId);
+        // 입력 된 시간이 현재 시간보다 이전이면 예외처리
+        if (expireTime.isBefore(LocalDateTime.now())) {
+            throw new CustomException(CustomExceptionList.SCHEDULE_START_TIME_TOO_EARLY_ERROR);
+        }
+
+        String quizIdStr = SCHEDULE_PREFIX + quizId;
         op.set(quizIdStr, "");
 
         Duration expireDuration = Duration.between(LocalDateTime.now(), expireTime);
@@ -32,4 +44,46 @@ public class ScheduleReserveService {
         stringRedisTemplate.expire(quizIdStr, expireDuration);
     }
 
+    /**
+     *
+     * 아직 시작하지 않은 퀴즈의 시작 시간을 변경한다.
+     * 
+     * @param quizId 스케줄링 된 퀴즈의 ID
+     * @param expireTime 교체할 퀴즈 시작 시간
+     */
+    public void update(Long quizId, LocalDateTime expireTime) {
+
+        ValueOperations<String, String> op = stringRedisTemplate.opsForValue();
+
+        String quizIdStr = SCHEDULE_PREFIX + quizId;
+
+        // 입력 된 시간이 현재 시간보다 이전이면 예외처리
+        if (expireTime.isBefore(LocalDateTime.now())) {
+            throw new CustomException(CustomExceptionList.SCHEDULE_START_TIME_TOO_EARLY_ERROR);
+        }
+
+        if (op.get(quizIdStr) == null) {
+            throw new CustomException(CustomExceptionList.SCHEDULE_NOT_FOUND_ERROR);
+        }
+
+        Duration expireDuration = Duration.between(LocalDateTime.now(), expireTime);
+
+        stringRedisTemplate.expire(quizIdStr, expireDuration);
+    }
+
+    /**
+     * 
+     * 스케줄링 된 퀴즈를 취소한다
+     * 
+     * @param quizId 스케줄링 된 퀴즈 ID
+     */
+    public void delete(Long quizId){
+        ValueOperations<String, String> op = stringRedisTemplate.opsForValue();
+
+        String quizIdStr = SCHEDULE_PREFIX + quizId;
+
+        if (op.getAndDelete(quizIdStr) == null) {
+            throw new CustomException(CustomExceptionList.SCHEDULE_NOT_FOUND_ERROR);
+        }
+    }
 }
