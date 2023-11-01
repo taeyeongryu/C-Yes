1678d7ad7a26ffd97b6511a84769da8a723b5971
SuIn Woo <wbo1026@naver.com>
2023-11-01 17:17:29 +0900

    feat: redux persist 저장

--- Front/cyesfront/src/redux/store.ts
+++ Front/cyesfront/src/redux/store.ts
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./reducers/rootReducer";
+import persistedReducer from "./reducers/rootReducer";
 
 const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
 });
 
 export default store;
