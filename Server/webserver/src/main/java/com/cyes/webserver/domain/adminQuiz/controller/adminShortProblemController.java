e5982a2b90a70590e0e339bcffb2f0e9fb337f4a
handaldog <handal@kakao.com>
2023-11-03 15:13:22 +0900

    feat : [FE] quiz 페이지 생성

--- Server/webserver/src/main/java/com/cyes/webserver/domain/adminQuiz/controller/adminShortProblemController.java
+++ Server/webserver/src/main/java/com/cyes/webserver/domain/adminQuiz/controller/adminShortProblemController.java
@@ -2,11 +2,14 @@ package com.cyes.webserver.domain.adminQuiz.controller;
 
 
 import com.cyes.webserver.domain.adminQuiz.service.OpenAIPostRequestService;
+import com.cyes.webserver.domain.member.dto.MemberNicknameReq;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -22,15 +25,17 @@ public class adminShortProblemController {
 
     @Operation(summary = "문제 생성", description = "단답형 문제 생성을 도와주는 문제들")
     @GetMapping("/create/{word}")
-    public List<String> createShortProblem(@PathVariable("word") String word) throws JsonProcessingException {
+    public ResponseEntity<String> createShortProblem(@PathVariable("word") String word) throws JsonProcessingException {
         List<String> wordlist = openAIPostRequestService.sendPostword(word);
 
         String[] newStr = wordlist.get(0).split("\n");
         for (int i=0;i<newStr.length;i++){
-            System.out.println("didi : " + newStr[i]);
+
             openAIPostRequestService.makeShortProblem(newStr[i]);
         }
-        return wordlist;
+        return ResponseEntity.status(HttpStatus.OK).body("저장 성공");
 
     }
+
+    
 }
