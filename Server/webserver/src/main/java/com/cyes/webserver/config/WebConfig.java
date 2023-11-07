746d90cf98a018c094733356b175365d8a042076
jjunehee <jjhjjh1159@gmail.com>
2023-11-07 10:33:37 +0900

    Feat : Swagger Webconfig 설정

--- Server/webserver/src/main/java/com/cyes/webserver/config/WebConfig.java
+++ Server/webserver/src/main/java/com/cyes/webserver/config/WebConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -18,8 +19,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("http://localhost:9510", "https://k9b103.p.ssafy.io", "https://cyes.site")
-                .allowedMethods("OPTIONS","GET","POST","PUT","DELETE");
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/3.52.5/");
+    }
+
 
 //    @Override
 //    public void addInterceptors(InterceptorRegistry interceptorRegistry) {
