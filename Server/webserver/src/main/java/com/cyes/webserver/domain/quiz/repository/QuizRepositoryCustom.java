cc70ad00f61f6e63b237ee3dca502113f6ff6f87
jjunehee <jjhjjh1159@gmail.com>
2023-11-09 13:40:41 +0900

    Feat : 그룹 퀴즈 조회 API

--- Server/webserver/src/main/java/com/cyes/webserver/domain/quiz/repository/QuizRepositoryCustom.java
+++ Server/webserver/src/main/java/com/cyes/webserver/domain/quiz/repository/QuizRepositoryCustom.java
@@ -4,10 +4,13 @@ import com.cyes.webserver.domain.quiz.entity.Quiz;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface QuizRepositoryCustom {
 
     Optional<Quiz> findLiveQuiz(LocalDateTime nowDateTime);
+
+    Optional<List<Quiz>> findGroupQuiz(LocalDateTime nowDateTime);
 }
--- Server/webserver/src/main/java/com/cyes/webserver/domain/quiz/repository/QuizRepositoryCustomImpl.java
+++ Server/webserver/src/main/java/com/cyes/webserver/domain/quiz/repository/QuizRepositoryCustomImpl.java
@@ -1,14 +1,15 @@
 package com.cyes.webserver.domain.quiz.repository;
 
+import com.cyes.webserver.domain.member.enums.MemberAuthority;
 import com.cyes.webserver.domain.quiz.entity.QQuiz;
 import com.cyes.webserver.domain.quiz.entity.Quiz;
-import com.querydsl.core.types.Order;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -23,14 +24,39 @@ public class QuizRepositoryCustomImpl implements QuizRepositoryCustom {
     @Override
     public Optional<Quiz> findLiveQuiz(LocalDateTime nowDateTime) {
         QQuiz quiz = QQuiz.quiz;
-        
+
         LocalDateTime todayEnd = nowDateTime.withHour(23).withMinute(59).withSecond(59).withNano(999_999_999);
 
         return Optional.ofNullable(jpaQueryFactory
                 .select(quiz)
                 .from(quiz)
-                .where(quiz.startDateTime.between(nowDateTime, todayEnd))
+                .where(isTimeBetween(nowDateTime, quiz, todayEnd))
                 .orderBy(quiz.startDateTime.asc())
                 .fetchFirst());
     }
+
+    @Override
+    public Optional<List<Quiz>> findGroupQuiz(LocalDateTime nowDateTime) {
+
+        QQuiz quiz = QQuiz.quiz;
+
+        LocalDateTime todayEnd = nowDateTime.withHour(23).withMinute(59).withSecond(59).withNano(999_999_999);
+
+       return Optional.ofNullable(jpaQueryFactory
+                .select(quiz)
+                .from(quiz)
+                .where(isUser(quiz).and(isTimeBetween(nowDateTime, quiz, todayEnd)))
+                .orderBy(quiz.startDateTime.asc())
+                .fetch());
+    }
+
+    private static BooleanExpression isUser(QQuiz quiz) {
+        return quiz.member.memberAuthority.eq(MemberAuthority.USER);
+    }
+
+    private static BooleanExpression isTimeBetween(LocalDateTime nowDateTime, QQuiz quiz, LocalDateTime todayEnd) {
+        return quiz.startDateTime.between(nowDateTime, todayEnd);
+    }
+
+
 }
