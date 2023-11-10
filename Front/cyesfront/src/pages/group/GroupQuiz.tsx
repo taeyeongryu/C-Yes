9dbdefbf32e9c3a31fcd14f8ba158f94c3ea1d1e
subin <qotnqls77@naver.com>
2023-11-10 13:16:45 +0900

    style: group quiz 수정

--- Front/cyesfront/src/pages/group/GroupQuiz.tsx
+++ Front/cyesfront/src/pages/group/GroupQuiz.tsx
@@ -3,30 +3,96 @@ import BottomNav from "../../components/bottomnav/BottomNav";
 import Dropdown from "../../components/dropdown/Dropdown";
 import RoundCornerBtn from "../../components/RoundCornerBtn";
 import "./GroupQuiz.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { getCsCategory } from "../../api/CsAPI";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 type Props = {};
+type QuizInfo = {
+  quizName: string;
+  quizType: string;
+  quizSubject: string;
+  quizNumber: number;
+  quizStartTime: string;
+};
 
 const GroupQuiz = (props: Props) => {
-  const [view, setView] = useState(false);
-  const items = ["항목 1", "항목 2", "항목 3"];
-  const [number, setNumber] = useState<number>(0);
+  const types = ["TF 퀴즈", "객관식", "단답형"];
+  const [categories, setCategories] = useState<string[]>([]);
 
-  const handleIncrement = () => {
-    setNumber(number + 1);
+  const [quizName, setQuizName] = useState<string>("");
+  const [quizType, setQuizType] = useState("");
+  const [quizSubject, setQuizSubject] = useState("");
+  const [number, setNumber] = useState<number>(0);
+  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+  const navigate = useNavigate();
+
+  const handleNextButtonClick = () => {
+    const quizInfo: QuizInfo = {
+      quizName,
+      quizType,
+      quizSubject,
+      quizNumber: number,
+      quizStartTime: formatDate(selectedDate),
+    };
+    navigate("/group");
+
+    // history.push("/nextpage", quizInfo);
   };
 
-  const handleDecrement = () => {
-    if (number > 0) {
-      setNumber(number - 1);
+  // API
+  const fetchCategories = async () => {
+    const data = await getCsCategory(); // API 호출
+    if (data) {
+      setCategories(data);
     }
   };
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+  // useEffect
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const handleDateChange = (date: Date | null) => {
+    setSelectedDate(date);
+  };
+
+  const formatDate = (date: Date | null) => {
+    if (date) {
+      return date.toISOString();
+    }
+    return "";
+  };
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const input = event.target.value;
+    setQuizName(input);
+  };
+  const handleTypeChange = (selectedValue: string) => {
+    setQuizType(selectedValue);
+  };
+
+  const handleSubjectChange = (selectedValue: string) => {
+    setQuizSubject(selectedValue);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const input = event.target.value;
+
     if (!isNaN(Number(input))) {
       setNumber(Number(input));
     }
+
+    if (Number(input) > 10) {
+      alert("문제 갯수는 최대 10개까지 가능합니다.");
+      setNumber(10);
+    } else {
+      setNumber(Number(input));
+    }
   };
+
   return (
     <div className="live-container">
       <div className="content">
@@ -37,37 +103,58 @@ const GroupQuiz = (props: Props) => {
           <div className="dropdown-content">
             <div>퀴즈이름</div>
             <div>
-              <textarea className="textarea" name="" id=""></textarea>
+              <input
+                className="textarea"
+                value={quizName}
+                onChange={handleTitleChange}
+              />
             </div>
           </div>
           <div className="dropdown-content">
             <div>문제유형</div>
             <div>
-              <Dropdown items={items} />
+              <Dropdown items={types} onChange={handleTypeChange} />
             </div>
           </div>
 
           <div className="dropdown-content">
             <div>퀴즈과목</div>
             <div>
-              <Dropdown items={items} />
+              <Dropdown items={categories} onChange={handleSubjectChange} />
             </div>
           </div>
           <div className="dropdown-content">
             <div>문제 갯수</div>
             <div>
-              <textarea
+              <input
                 className="textarea"
                 value={number}
                 onChange={handleInputChange}
+                type="number"
               />
             </div>
+          </div>
+          <div className="dropdown-content">
+            <div>시작 시간</div>
+            <DatePicker
+              selected={selectedDate}
+              onChange={handleDateChange}
+              showTimeSelect
+              dateFormat="yyyy-MM-dd HH:mm:ss"
+              timeFormat="HH:mm:ss"
+              // customInput={<div style={customStyles.datePicker} />}
+            />
           </div>
 
           <br />
           <br />
-          <RoundCornerBtn>취소</RoundCornerBtn>
-          <RoundCornerBtn>다음</RoundCornerBtn>
+          <div className="group-button-content">
+            <RoundCornerBtn>취소</RoundCornerBtn>
+            <div className="box"></div>
+            <RoundCornerBtn onClick={handleNextButtonClick}>
+              다음
+            </RoundCornerBtn>
+          </div>
         </div>
       </div>
       <BottomNav checkCS={false} checkLive={false} checkGroup={true} />
