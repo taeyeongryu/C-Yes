96d9a4070a4853ed2009385bec2322521cc212c8
dntmdqls0912 <dntmdqls0912@gmail.com>
2023-11-10 17:47:44 +0900

    feat: 퀴즈 submitox 추가

--- Front/cyesfront/src/components/RoundCornerBtn.tsx
+++ Front/cyesfront/src/components/RoundCornerBtn.tsx
@@ -2,37 +2,40 @@ import React from "react";
 import styled from "styled-components";
 
 type Props = {
-  children?: React.ReactNode;
-  width?: string;
-  height?: string;
-  bgcolor?: string;
-  bghover?: string;
-  fontSize?: string;
-  fontcolor?: string;
-  type?: "button" | "submit" | "reset"; // type 속성 추가
-  onClick?: () => void;
-  disabled?: boolean;
-  customshadow?: string;
+    children?: React.ReactNode;
+    width?: string;
+    height?: string;
+    bgcolor?: string;
+    bghover?: string;
+    fontSize?: string;
+    fontcolor?: string;
+    type?: "button" | "submit" | "reset"; // type 속성 추가
+    onClick?: () => void;
+    disabled?: boolean;
+    customshadow?: string;
+    opacity?: number;
 };
 
 const StyledButton = styled.button<Props>`
-  width: ${(props) => props.width || "100px"};
-  height: ${(props) => props.height || `40px`};
-  background-color: ${(props) => props.bgcolor || `#31a0eb`};
-  &:active {
-    background-color: ${(props) => props.bghover || `#32237d`};
-  }
-  font-size: ${(props) => props.fontSize || "12px"};
-  color: ${(props) => props.fontcolor || `#ffffff`};
-  border-radius: 18px;
-  border: none;
-  font-family: "Dovemayo_gothic";
-  box-shadow: ${(props) => props.customshadow || "0 4px 6px rgba(0, 0, 0, 0.1)"}; // 커스텀 그림자 스타일
-  
+    width: ${(props) => props.width || "100px"};
+    height: ${(props) => props.height || `40px`};
+    background-color: ${(props) => props.bgcolor || `#31a0eb`};
+    &:active {
+        background-color: ${(props) => props.bghover || `#32237d`};
+    }
+    font-size: ${(props) => props.fontSize || "12px"};
+    color: ${(props) => props.fontcolor || `#ffffff`};
+    border-radius: 18px;
+    border: none;
+    font-family: "Dovemayo_gothic";
+    box-shadow: ${(props) =>
+        props.customshadow ||
+        "0 4px 6px rgba(0, 0, 0, 0.1)"}; // 커스텀 그림자 스타일
+    opacity: ${(props) => props.opacity || 1};
 `;
 
 const RoundCornerBtn = ({ children, ...style }: Props) => {
-  return <StyledButton {...style}>{children}</StyledButton>;
+    return <StyledButton {...style}>{children}</StyledButton>;
 };
 
 export default RoundCornerBtn;
