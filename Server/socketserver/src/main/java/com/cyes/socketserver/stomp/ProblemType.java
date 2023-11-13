514ddc8d8195800ca155d77490cf08738e57ce8f
유태영 <tybam@naver.com>
2023-11-13 16:35:54 +0900

    Fix: Problem Message

---
+++ Server/socketserver/src/main/java/com/cyes/socketserver/stomp/ProblemType.java
@@ -0,0 +1,6 @@
+package com.cyes.socketserver.stomp;
+
+public enum ProblemType {
+    //객관식, OX, 단답형
+    MULTIPLECHOICE, TRUEORFALSE, SHORTANSWER
+}
--- Server/socketserver/src/main/java/com/cyes/socketserver/stomp/dto/SubmitRedis.java
+++ Server/socketserver/src/main/java/com/cyes/socketserver/stomp/dto/SubmitRedis.java
@@ -4,7 +4,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.ToString;
-import org.springframework.data.redis.core.RedisHash;
 
 
 @Data
